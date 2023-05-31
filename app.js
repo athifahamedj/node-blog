461099ff0a8988604262d7ff78cafc04e8ac783a
athif <athifahamedj@gmail.com>
2023-05-31 18:45:24 +0530

    adding folders

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const PORT = 3001;
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+
+app.use(express.static("public"));
+
+// template engine
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+  const locals = {
+    title: "NodeJS Blog",
+    desc: "Created with NodeJS, Express and MongoDB",
+  };
+  res.render("index", locals);
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Port ${PORT} working`);
+});
